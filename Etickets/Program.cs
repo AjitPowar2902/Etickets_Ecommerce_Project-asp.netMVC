65bc0fd50e86e65454265bfc5a69edc2c2f75150
Ajit2902 <ajitp2902@gmail.com>
2023-04-03 22:20:35 +0530

    First Commit

--- Etickets/Program.cs
+++ Etickets/Program.cs
@@ -1,7 +1,14 @@
+using Etickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DbContext Configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
